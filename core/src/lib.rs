fe9322b8c42c076aba9683a6aee0351aaa18b58d
Artem Yarulin <artem.yarulin@kapteko.com>
2024-05-30 12:32:00 +0300

    Refresh logic version in runtime, use interior mutability to avoid crashing

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,16 @@
+use std::cell::RefCell;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub struct Logic {
     pub ball_x: usize,
-    pub ball_y: usize,
     pub radius: usize,
-    pub dy: isize,
     pub mass: usize,
     pub gravity: usize,
-    pub frame: usize,
+
+    ball_y: RefCell<usize>,
+    frame: RefCell<usize>,
+    dy: RefCell<isize>,
     version: &'static str,
     display_height: usize,
 }
@@ -18,28 +20,44 @@ impl Logic {
     pub fn new(display_width: usize, display_height: usize) -> Self {
         Self {
             ball_x: display_width / 2,
-            ball_y: display_height / 2,
+            ball_y: RefCell::new(display_height / 2),
             radius: 20,
-            dy: 2,
+            dy: RefCell::new(2),
             mass: 1,
             gravity: 1,
-            frame: 0,
-            version: "1.1",
+            frame: RefCell::new(0),
+            version: "1.5",
             display_height,
         }
     }
 
-    pub fn tick(&mut self) {
-        self.dy += (self.gravity * self.mass) as isize;
-        self.ball_y += self.dy as usize;
-        if self.ball_y + self.radius > self.display_height {
-            self.ball_y = self.display_height - self.radius;
-            self.dy = -self.dy;
+    pub fn tick(&self) {
+        let mut dy = self.dy.borrow_mut();
+        let mut ball_y = self.ball_y.borrow_mut();
+        let mut frame = self.frame.borrow_mut();
+
+        *dy += (self.gravity * self.mass) as isize;
+        *ball_y += *dy as usize;
+        if *ball_y + self.radius > self.display_height {
+            *ball_y = self.display_height - self.radius;
+            *dy = -*dy;
         }
-        self.frame = if self.frame == 60 { 0 } else { self.frame + 1 };
+        *frame = if *frame == 60 { 0 } else { *frame + 1 };
     }
 
     pub fn version(&self) -> String {
         self.version.to_string()
+    }
+
+    pub fn dy(&self) -> isize {
+        self.dy.clone().take()
+    }
+
+    pub fn ball_y(&self) -> usize {
+        self.ball_y.clone().take()
+    }
+
+    pub fn frame(&self) -> usize {
+        self.frame.clone().take()
     }
 }
